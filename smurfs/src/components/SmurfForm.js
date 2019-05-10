4f91ae1463487b3d58caf649cdfd71a49a38c8ec
Greyflanel <richardlovelace811@gmail.com>
2019-05-10 15:36:35 -0500

    Rendered all of smurf server data, now I am taking a break.

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 class SmurfForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            age: null,
-            height: ""
-        }
-    }
-    render() {
-        
-        return (
-       <div>
-           {this.props.smurfs.map( smurf => smurf.name)}
-       </div>
-
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: null,
+      height: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.props.smurfs.map(smurf => (
+          <div>
+            <h2>{smurf.name}</h2>
+            <p>{smurf.age}</p>
+            <p>{smurf.height}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        smurfs: state.smurfs
-    }
-}
-export default connect(mapStateToProps) (SmurfForm)
+  return {
+    smurfs: state.smurfs
+  };
+};
+export default connect(mapStateToProps)(SmurfForm);
